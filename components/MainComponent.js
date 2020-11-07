8b8529b147d00a8b1a9cf7cc0b82f4b78666db4e
LordFagan <the.fagan@gmail.com>
2020-11-07 11:54:24 -0600

    Week 4 Assignment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -327,13 +327,7 @@ class Main extends Component {
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
-
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' +
-                    connectionInfo.type, ToastAndroid.LONG);
-        });
+        this.showNetInfo();
 
         this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
             this.handleConnectivityChange(connectionInfo);
@@ -363,6 +357,14 @@ class Main extends Component {
         (Platform.OS === 'ios')
             ? Alert.alert('Connection change:', connectionMsg)
             : ToastAndroid.show(connectionMsg, ToastAndroid.LONG);
+    }
+
+    async showNetInfo() {
+        const connectionInfo = await NetInfo.fetch()
+            (Platform.OS === 'ios')
+                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+                : ToastAndroid.show('Initial Network Connectivity Type: ' +
+                    connectionInfo.type, ToastAndroid.LONG);
     }
 
     render() {
